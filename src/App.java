c56d2ca3a8c1c99fa1d6c3e4bf8d93dfef5ef29e
MartinSilen <iokiokna@gmail.com>
2021-11-05 12:16:33 +0200

    Guess what?

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int Answer = rnd.nextInt(9) + 1;
+        Scanner com = new Scanner(System.in);
+        while (true) {
+            System.out.println("Guess the number:");
+            int inp = com.nextInt();
+            if (inp == Answer) {
+                System.out.println("That's Right!");
+                break;
+            } else System.out.println("Nope, try again");
+        }
+
+
+    }
+}
